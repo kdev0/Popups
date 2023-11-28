3310292e0e6760607a5e032ffa538a76afbe3613
Konstantin D <dev.konst@gmail.com>
2023-11-28 10:35:00 +0400

    content color
    - fixed content color on the popup

--- Sources/Popups/Views/PopupView.swift
+++ Sources/Popups/Views/PopupView.swift
@@ -21,17 +21,6 @@ struct PopupView<Content: View>: View {
             content
                 .frame(maxWidth: .infinity)
         }
-//        .ignoresSafeArea(.all, edges: .bottom)
-        .clipShape(RoundedCorner(radius: 20, corners: [.topLeft, .topRight]), style: .init(eoFill: true, antialiased: true))
-        .background(content.background(ignoresSafeAreaEdges: .bottom).frame(maxWidth: .infinity)).ignoresSafeArea(edges: .bottom)
-//        .background(
-////            content
-//            Color.white
-//            .clipShape(RoundedCorner(radius: 20, corners: [.topLeft, .topRight]))
-//            .ignoresSafeArea(edges: .bottom)
-//        )
-//        .background(content.ba\)
-
     }
 }
 
